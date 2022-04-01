252defd09e5f29b9dc08b66021e8a32b4ca24740
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-04-01 12:12:35 -0300

    :label: adicionando typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./index.css";
 import React, { useState, useEffect } from "react";
 
+interface DataProps {
+  id: number;
+  name: string;
+  title: string;
+  description: string;
+  image: string;
+  actualData: Array<string>;
+}
+
 const App = () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState();
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
@@ -17,12 +26,12 @@ const App = () => {
             `Este é um erro HTTP, o status é: ${response.status}`
           );
         }
-        const actualData = await response.json();
+        const actualData: string[] = await response.json();
         setData(actualData);
-        setError();
+        setError(null);
       } catch (err) {
         setError(err.message);
-        setData();
+        setData(null);
       } finally {
         setLoading(false);
       }
@@ -39,7 +48,7 @@ const App = () => {
       )}
       <article>
         {data &&
-          data.map(({ id, title, description, image }) => (
+          data.map(({ id, title, description, image }: DataProps) => (
             <span key={id}>
               <img src={image} alt={title} />
               <h2>{title}</h2>
